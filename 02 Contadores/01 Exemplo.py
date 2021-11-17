9531a7ee7bb720f95b3655f23d092b1951eda34e
Tiago Bezerra <tdtiagouni2@gmail.com>
2021-11-17 20:10:45 -0300

    Treinando contadores

---
+++ 02 Contadores/01 Exemplo.py
@@ -0,0 +1,6 @@
+#5.1 Contadores
+fim=int(input('Digite o ultimo número a imprimir:'))
+x=1
+while x<=fim:
+    print(x)
+    x=x+1
---
+++ 02 Contadores/02 Exemplo2.py
@@ -0,0 +1,6 @@
+#descobrindo se o número é par ou ímpar
+
+x=int(input('Digite o número que deseja saber se é impar ou par: '))
+if x%2==0:
+    print(f'O número {x} é par.')
+else: print(f'O número {x} é ímpar')
