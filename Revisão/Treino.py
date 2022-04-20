e46d061e6977fa1e6b76d612bdd2a4d17a75fcfc
Tiago Bezerra <tdtiagouni2@gmail.com>
2022-04-19 22:25:29 -0300

    Revisão

---
+++ Revisão/Treino.py
@@ -0,0 +1,16 @@
+print('Retornando')
+a=10
+b=5
+soma=a+b
+subtração= a-b
+multiplicação = a*b
+divisão = a/b
+resto = 10%5
+
+print(type(soma))
+soma= str(soma)
+print(type(soma))
+print(subtração)
+print(multiplicação)
+print(divisão )
+print(resto)
